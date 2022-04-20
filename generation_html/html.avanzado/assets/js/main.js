d32752761dfc9c47c86b1a081054676675fc661d
PedroAntonioSC7710 <pedro.antonio.sanchez771@gmail.com>
2022-04-20 11:43:36 -0500

    agregamos javascript a nuestro formulario

---
+++ generation_html/html.avanzado/assets/js/main.js
@@ -0,0 +1,107 @@
+const color = document.querySelector("#inputColor")
+        console.log(color);
+
+        color.addEventListener("change", function (e){
+            document.body.style.backgroundColor = e.target.value;
+        })
+
+const formulario = document.querySelector("#formulario datos")
+console.log(formulario)
+
+formulario,addEventListener("submit", function (e) {
+    e.preventDefault()
+    const datos = Object.fromEntries(
+        new FormData(e.target)
+    )
+    console.log(datos)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
