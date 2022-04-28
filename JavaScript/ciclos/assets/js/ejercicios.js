a46fe9dbf56d401cb46d3c81538c68d7d5456c4d
PedroAntonioSC7710 <pedro.antonio.sanchez771@gmail.com>
2022-04-28 15:52:13 -0500

    Agregada carpeta de ciclos

---
+++ JavaScript/ciclos/assets/js/ejercicios.js
@@ -0,0 +1,148 @@
+//escribe un ciclo anidado que imprima el siguiente patron
+
+/* 
+
+*
+**
+***
+****
+*****
+
+*/
+
+//for(let i = 1; i<=5; i++){      //1  //2
+  //  for(let j = 1; j<=i; j++){  //5  //5
+    //    document.write('*');
+        //console.log('ciclo hijo\n');
+   // }
+    
+   // document.write('<br>');
+
+//}
+
+
+/* let numeroPositivo = 10
+do {
+    console.log(numeroPositivo);
+    numeroPositivo=numeroPositivo-0.5
+
+} while (numeroPositivo);  */
+
+
+/* let modulo;
+
+for (let i = 1; i < 100; i++){
+    if(i%2==0){
+
+    }
+    else{
+        console.log('El nunero '+i+' es impar');
+    }
+} */
+
+
+/* for (let i = 0; i <= 100; i++) {
+    if(i%2!=0) {
+        console.log('El numero ' + i + ' es impar');
+    }
+} */
+
+
+/* let n = 6
+let contador=1;
+while (contador<=n) {
+    document.write("["+contador+"]");
+    contador++;
+}
+ */
+
+let n = 15;
+let suma = 0;
+for(let i = 1; i <= n; i++){
+    suma = suma + i;
+}
+
+console.log(suma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/ciclos/assets/js/main.js
@@ -0,0 +1,126 @@
+/* Ciclos */
+
+//for
+//for ...in
+//.forEach
+
+
+//imprimir la frase "hola mundo" 10 veces
+//(inicializamos la variable; condición; modificador del contador)
+//for(let i = 1; i<=10; i++){
+    //console.log("Hola mundo");
+    //debugger;
+//}
+//iteración= una cuelta de cada ciclo
+//console.log(i);
+
+
+//While
+
+let control = 1;//control
+
+//while (control <= 5) {//condicion a observar 
+
+    //console.log("Hola desde un ciclo while", control);
+
+    //control++;//modificador de la variable de control
+
+    //debugger;
+//}
+
+//console.log();
+
+
+//do...while
+
+let i = 1;
+
+do {
+    console.log("hola desde un do..while", i);
+    i++;
+
+} while (i <= 5); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
