cb5aec026728c980c8d0fd6f03d6e58fe402c9c7
PedroAntonioSC7710 <pedro.antonio.sanchez771@gmail.com>
2022-05-04 17:55:12 -0500

    creamos una pagina de anime con apis

---
+++ JavaScript/jikan/assets/js/buscador.js
@@ -0,0 +1,50 @@
+const formulario = document.querySelector("form")
+console.log(formulario);
+
+formulario.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    /* console.log("boton presionado"); */
+
+    const busqueda =document.querySelector("input").value
+    console.log(busqueda);
+
+    window.location.href = `busqueda.html?nombre=${busqueda}`
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
