30a20c4cbca999f60291d18a893602f4c17cf692
PedroAntonioSC7710 <pedro.antonio.sanchez771@gmail.com>
2022-05-09 15:11:43 -0500

    introduccion a pruebas unitarias

---
+++ JavaScript/pruebas_unitarias/assets/js/app.js
@@ -0,0 +1,124 @@
+const suma = (a, b) => {
+    return a - b;
+}
+
+
+/* console.log((suma(1,3) === 4));
+console.log((suma(0,0) === 0));
+console.log((suma(5,5) === 10)); */
+
+//? :
+
+/* console.assert(
+    suma(1,3) === 4,
+    'el resultado de la suma de 1 y 3 deveria ser 4'
+);
+
+console.assert(suma(1,3) === 4, 'el resultado de la suma de 1 y 3 deveria ser 4'
+); */
+
+const arr = [
+    {a:1, b:3, resultado: 4},
+    {a:0, b:0, resultado: 0},
+    {a:5, b:5, resultado: 10},
+    {a:10, b:30, resultado: 40}
+]
+
+arr.forEach(prueba => {
+    const { a , b, resultado } = prueba;//destructuracion
+    console.assert(suma(a, b) === resultado, 
+    `Suma de ${a} y ${b} deveria ser ${resultado}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
